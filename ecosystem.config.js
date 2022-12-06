49d18d4aae44eb7224648faf70248d6bb63d257b
Dede Gunawan <gunawan@dede.web.id>
2022-12-06 11:16:58 +0700

    Add ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps : [
+      {
+          name   : "node-proxy",
+          script : "./index.js",
+          env_dev: {
+              NODE_ENV: "dev"
+          }
+      },
+      {
+          name   : "node-proxy-bsi",
+          script : "./index.js",
+          env_development: {
+              NODE_ENV: "bsi-dev"
+          }
+      },
+      {
+          name   : "node-proxy-bni",
+          script : "./index.js",
+          env_development: {
+              NODE_ENV: "bni-dev"
+          }
+      },
+  ]
+}
